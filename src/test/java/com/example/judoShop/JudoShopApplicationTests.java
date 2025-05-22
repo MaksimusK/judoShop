2b051b764f0712e3f0417b2f452c468da82b826a
MaksumusK <maksimusstalker2003@gmail.com>
2025-05-22 17:10:44 +0700

    Initial commit

---
+++ src/test/java/com/example/judoShop/JudoShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.judoShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JudoShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
