bbdb6debccc06b121251cd226bc70b4f08aa08a4
MaksumusK <maksimusstalker2003@gmail.com>
2025-06-09 12:27:41 +0700

    создание роли админа

--- src/main/java/com/example/judoShop/models/User.java
+++ src/main/java/com/example/judoShop/models/User.java
@@ -61,6 +61,11 @@ public class User implements UserDetails {
 
     //security
 
+
+    public boolean isAdmin() {
+        return roles.contains(Role.ROLE_ADMIN);
+    }
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return roles;
